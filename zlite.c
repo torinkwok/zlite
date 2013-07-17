4ae5f92b1381352a3e3a767fb57fb31c17d84309
richox <richselian@gmail.com>
2013-07-18 00:34:17 +0800

    optimized.

--- zlite.c
+++ zlite.c
@@ -38,7 +38,7 @@
 #include <io.h>
 
 int ma_n(int argc, char** argv);
-int main(int argc, char** argv) { // set stdio to binary mode for windows
+int main(int argc, char** argv) { /* set stdio to binary mode for windows */
     setmode(fileno(stdin),  O_BINARY);
     setmode(fileno(stdout), O_BINARY);
     return ma_n(argc, argv);
@@ -72,7 +72,7 @@ MakeTablePass:
     for(i = 0; i < POLAR_SYMBOLS; i++) {
         symbols[i] = i;
     }
-    for(i = 0; i < POLAR_SYMBOLS; i++) {
+    for(i = 0; i < POLAR_SYMBOLS; i++) { /* simple gnome sort */
         if(i > 0 && leng_table[symbols[i - 1]] < leng_table[symbols[i]]) {
             M_int_swap(symbols[i - 1], symbols[i]);
             i -= 2;
@@ -182,7 +182,7 @@ int polar_make_decode_table(const int* leng_table, const int* code_table, int* d
 #define ROLZ_BUCKET_SIZE    65536
 #define MATCH_IDX_SIZE      15  /* make element of rolz_table[] 64 Bytes */
 #define MATCH_LEN_MIN       2
-#define MATCH_LEN_MAX       17  /* MATCH_LEN_MAX < MATCH_LEN_MIN + (POLAR_SYMBOLS-256) / MATCH_IDX_SIZE */
+#define MATCH_LEN_MAX       17  /* MATCH_LEN_MAX < MATCH_LEN_MIN + (POLAR_SYMBOLS-257) / MATCH_IDX_SIZE */
 
 static unsigned short lastword = 0;
 static unsigned short context = 0;
@@ -193,17 +193,29 @@ static const unsigned char __cache_aligned(mod15_table)[] = { /* MATCH_IDX_SIZE=
 };
 #define M_rolz_item(x, n) rolz_table[(x)].m_item[mod15_table[rolz_table[(x)].m_head + (n)]]
 
+/* last-char table */
+static unsigned char lastchar[65536];
+
+/* rolz table */
 static struct {
     unsigned int m_item[MATCH_IDX_SIZE];
     unsigned int m_head;
-}__cache_aligned(rolz_table)[ROLZ_BUCKET_SIZE];
+} __cache_aligned(rolz_table)[ROLZ_BUCKET_SIZE];
 
 static inline void rolz_update_context(unsigned char* buf, int pos, int cache) {
-    rolz_table[context].m_head = mod15_table[rolz_table[context].m_head + MATCH_IDX_SIZE - 1];
+    int new_head;
 
-    rolz_table[context].m_item[rolz_table[context].m_head] = cache ?
+    /* update rolz table */
+    new_head = mod15_table[rolz_table[context].m_head + MATCH_IDX_SIZE - 1];
+    rolz_table[context].m_head = new_head;
+    rolz_table[context].m_item[new_head] = cache ?
         pos | (buf[pos] << 24) :
         pos;
+
+    /* update last-char table */
+    lastchar[lastword] = buf[pos];
+
+    /* update context */
     context = lastword * 13131 + buf[pos];
     lastword <<= 8;
     lastword |= buf[pos];
@@ -217,13 +229,11 @@ int rolz_encode(unsigned char* ibuf, unsigned short* obuf, int ilen) {
     int j;
     int match_idx;
     int match_len;
-    unsigned int item;
-    unsigned int item_chr;
-    unsigned int item_pos;
+    unsigned int  item;
+    unsigned char item_chr;
+    unsigned int  item_pos;
 
     memset(rolz_table, 0, sizeof(rolz_table));
-    context = 0;
-    lastword = 0;
 
     while(pos < ilen) {
         match_len = MATCH_LEN_MIN - 1;
@@ -255,7 +265,11 @@ int rolz_encode(unsigned char* ibuf, unsigned short* obuf, int ilen) {
         }
 
         if(match_len < MATCH_LEN_MIN) { /* encode */
-            obuf[olen++] = ibuf[pos];
+            if(lastchar[lastword] != ibuf[pos]) { /* last-char match? */
+                obuf[olen++] = ibuf[pos];
+            } else {
+                obuf[olen++] = 511;
+            }
             rolz_update_context(ibuf, pos++, 1);
 
         } else {
@@ -276,11 +290,12 @@ int rolz_decode(unsigned short* ibuf, unsigned char* obuf, int ilen) {
     int match_len;
     int match_offset;
 
-    context = 0;
-    lastword = 0;
-
     for(pos = 0; pos < ilen; pos++) {
-        if(ibuf[pos] < 256) { /* process a literal byte */
+        if(ibuf[pos] == 511) { /* last-char match */
+            obuf[olen] = lastchar[lastword];
+            rolz_update_context(obuf, olen++, 0);
+
+        } else if(ibuf[pos] < 256) { /* process a literal byte */
             obuf[olen] = ibuf[pos];
             rolz_update_context(obuf, olen++, 0);
 
